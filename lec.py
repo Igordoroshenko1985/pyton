c56571f112bc64733610fef723bd1e3c1211ea02
unknown <igor.d85@mail.ru>
2022-09-23 16:26:11 +0300

    домашка по питону сем 1

---
+++ lec.py
@@ -0,0 +1,29 @@
+# print('hello world')
+# value = None
+# a= 123
+# b = 1.23
+# print(type(a))
+# print (type(b))
+# value = 12334
+# print(type(value))
+# print('Введите a');
+# a= int (input())
+# print('введите число ')
+# a = int(input())
+# print ('введите число b')
+# b = int (input())
+# if b
+# n = int(input(n));
+# while  (-n < i < n) 
+#   i= 0
+#   -n +i = count
+#   i+=i
+# print (count)
+print('введите число n')
+n = int(input())
+if n==7 or n==6:
+  print('выходной')
+elif 1 <= n <=5:
+  print ('будни')
+else:
+  print('не день недели')    
